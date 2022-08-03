e4c9d13d3041a20b08ccb395b60ed7e42523401a
Juan Augusto <83130876+Juan-Augusto@users.noreply.github.com>
2022-08-03 10:24:38 -0300

    First commit

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+import { Card, Typography } from "antd";
+
+
+export const Home = () => {
+    const { Title } = Typography;
+    return (
+        <Card>
+            <Title level={5}>Home</Title>
+        </Card>
+    );
+}
