ef8337bc5ea8a228cb1127ef84af9045f6df8fb2
Juan Augusto <83130876+Juan-Augusto@users.noreply.github.com>
2022-08-06 15:00:34 -0300

    Feat: main skill cards

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,7 +1,7 @@
 import { Button, Card, Typography, Row, Col, Layout, Image, Space, Divider } from "antd";
 import avatar from "../../Assets/avatar.png";
 import { useState } from "react";
-import { CodeOutlined, DatabaseOutlined } from "@ant-design/icons/lib/icons";
+import { CodeOutlined, DatabaseOutlined, ClusterOutlined } from "@ant-design/icons/lib/icons";
 
 export const Home = () => {
     const { Title, Text } = Typography;
@@ -14,13 +14,59 @@ export const Home = () => {
     const skillsSection = [
         {
             icon: <CodeOutlined />,
+            description: "Frontend is my main skill currently, is what I've studied and practiced the most in the last years.",
             title: "Frontend",
-            skills: []
+            languages: [
+                "Javascript", 
+                "CSS", 
+                "Typescript",
+                "Python",
+                "C",
+            ],
+            tools: [
+                "ReactJS",
+                "React Native",
+                "Bootstrap",
+                "Material UI",
+                "Ant Design",
+            ]
         },
         {
             icon: <DatabaseOutlined />,
+            description: "I've been working with databases and API development for more than a year now, and I'm always looking for new ways to improve my skills.",
             title: "Backend",
-            skills: []
+            languages: [
+                "NodeJS",
+                "Python",
+                "MySQL",
+                "MariaDB",
+            ],
+            tools: [
+                "ExpressJS",
+                "Mysql2",
+                "Sequelize",
+                "DBeaver",
+                "Postman",
+            ]
+        },
+        {
+            icon: <ClusterOutlined />,
+            description: "Frontend is my main skill currently, is what I've studied and practiced the most in the last years.",
+            title: "Extra Skills",
+            languages: [
+                "Javascript", 
+                "CSS", 
+                "Typescript",
+                "Python",
+                "C",
+            ],
+            tools: [
+                "ReactJS",
+                "React Native",
+                "Bootstrap",
+                "Material UI",
+                "Ant Design",
+            ]
         }
     ]
 
@@ -39,7 +85,7 @@ export const Home = () => {
             <Row justify="center" style={{marginBottom: '100px'}}>
                 <Image src={avatar} preview={false}/>
             </Row>
-            <Card style={{backgroundColor: '#9D2053', color: '#fff', padding: '70px', height: '500px'}}>
+            <Card style={{backgroundColor: '#9D2053', color: '#fff', padding: '70px', height: '350px'}}>
                 <Row justify="center">
                     <Title level={3} style={{fontFamily: 'Source Code Pro, monospace', color: '#fff'}}>
                         Hey, my name is Juan, I'm glad you're here.
@@ -56,17 +102,48 @@ export const Home = () => {
                     </Col>
                 </Row>
             </Card>
-            <Row justify="center" style={{marginTop: '-50px'}}>
-                    {skillsSection.map((skill, index) => {
-                        return(
-                            <Col span={8} key={index}>
-                                <Card style={{border: 'none', backgroundColor: '#111625'}}>
+            <Row justify="center" style={{marginTop: '-50px'}} gutter={16}>
+                {skillsSection.map((skill, index) => {
+                    return(
+                        <Col span={7} key={index}>
+                            <Card style={{border: 'none', backgroundColor: '#111625', borderRadius: '15px', height: '550px'}}>
+                                <Row justify="center">
+                                    <Title level={1} style={{color: '#fff', backgroundColor: '#9D2053', clipPath: 'circle()', padding: '15px'}}>{skill.icon}</Title>
+                                </Row>
+                                <Row justify="center">
                                     <Title level={4} style={{color: '#fff'}}>{skill.title}</Title>
-                                </Card>
-                            </Col>    
-                        )
-                    })}            
-
+                                </Row>
+                                <Row justify="center">
+                                    <Col span={18} style={{textAlign: 'center'}}>
+                                        <Text style={{color: '#fff'}}>{skill.description}</Text>
+                                    </Col>
+                                </Row>
+                                <Row justify="center" style={{marginTop: '20px'}}>
+                                    <Title level={4} style={{color: '#9D2053'}}>Main languages</Title>
+                                </Row>
+                                <Row justify="center">
+                                    <Space split={<Divider type="vertical" style={{backgroundColor: '#fff'}} />} wrap>
+                                            {skill.languages.map((language, index) => {
+                                                return(
+                                                    <Text style={{color: '#fff'}} key={index}>{language}</Text>
+                                                );
+                                            })} 
+                                    </Space>
+                                </Row>
+                                <Row justify="center" style={{marginTop: '20px'}}>
+                                    <Title level={4} style={{color: '#9D2053'}}>Main tools</Title>
+                                </Row>
+                                {skill.tools.map((tool, index) => {
+                                    return(
+                                        <Row justify="center" key={index}>
+                                            <Text level={4} style={{color: '#fff'}}>{tool}</Text>
+                                        </Row>
+                                    );
+                                })}
+                            </Card>
+                        </Col>    
+                    )
+                })}            
             </Row>
             <Row>
                 complete
